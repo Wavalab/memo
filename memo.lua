f8c0dcf61f1921cf7ac7c3d5f9baa8a1312b74bb
John Chin-Jew <jchinjew@gmail.com>
2017-11-05 01:34:53 -0700

    Added current state retrieval

--- memo.lua
+++ memo.lua
@@ -5,13 +5,16 @@
 --]]
 
 local memo = {}
+local state = {} -- Store current/most recent event data for each channel
 
 -- Publish a global event on @channel with @data
 function memo.pub(channel, data)
   Runtime:dispatchEvent({name = channel, data = data})
+  state[channel] = data
 end
 
 -- Subscribe function @fn to @channel
+-- Can access data via `event.data` @fn args `function(event)`
 function memo.sub(channel, fn)
   Runtime:addEventListener(channel, fn)
 end
@@ -22,6 +25,7 @@ function memo.unsub(channel, fn)
 end
 
 -- Subscribe @object to @channel with function @fn
+-- Can access data via `event.data` @fn args `function(self, event)`
 function memo.subo(object, channel, fn)
   object[channel] = fn
   Runtime:addEventListener(channel, object)
@@ -30,6 +34,11 @@ end
 -- Unsubscribe @object from @channel
 function memo.unsubo(object, channel)
   Runtime:removeEventListener(channel, object)
+end
+
+-- Access the most recent `event.data` for @channel
+function memo.stateOf(channel)
+  return state[channel]
 end
 
 return memo
