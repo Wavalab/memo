f18efebc56d2617e57a928328b6a14f8d13e4b7d
John Chin-Jew <jchinjew@gmail.com>
2017-11-05 09:25:25 -0800

    Proper spelling

--- memo.lua
+++ memo.lua
@@ -10,20 +10,20 @@
 local memo = {}
 local state = {}
 
-function memo.pub(ev, payload)
+function memo.publish(ev, payload)
   Runtime:dispatchEvent({name = ev, data = payload})
   state[ev] = data
 end
 
-function memo.subRF(ev, fn) Runtime:addEventListener(ev, fn) end
-function memo.subRT(ev, tbl, fn) tbl[ev] = fn; Runtime:addEventListener(ev, tbl) end
-function memo.subLF(ev, tbl, fn) tbl:addEventListener(ev, fn) end
-function memo.subLT(ev, tbl, fn) tbl[ev] = fn; tbl:addEventListener(ev) end
+function memo.subscribeRF(ev, fn) Runtime:addEventListener(ev, fn) end
+function memo.subscribeRT(ev, tbl, fn) tbl[ev] = fn; Runtime:addEventListener(ev, tbl) end
+function memo.subscribeLF(ev, tbl, fn) tbl:addEventListener(ev, fn) end
+function memo.subscribeLT(ev, tbl, fn) tbl[ev] = fn; tbl:addEventListener(ev) end
 
-function memo.unsubRF(ev, fn) Runtime:removeEventListener(ev, fn) end
-function memo.unsubRT(ev, tbl) Runtime:removeEventListener(ev, tbl) end
-function memo.unsubLF(ev, tbl, fn) tbl:removeEventListener(ev, fn) end
-function memo.unsubLT(ev, tbl) tbl:removeEventListener(ev) end
+function memo.unsubscribeRF(ev, fn) Runtime:removeEventListener(ev, fn) end
+function memo.unsubscribeRT(ev, tbl) Runtime:removeEventListener(ev, tbl) end
+function memo.unsubscribeLF(ev, tbl, fn) tbl:removeEventListener(ev, fn) end
+function memo.unsubscribeLT(ev, tbl) tbl:removeEventListener(ev) end
 
 function memo.peak(ev)
   return state[ev]
